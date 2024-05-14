ff8d9f670f36b24ee24577d7392eb89b55c3f8b1
Huarada <121201067+Huarada@users.noreply.github.com>
2024-05-13 22:24:22 -0300

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -1,6 +1,6 @@
 
 #Given an integer array nums, 
-# return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# returns all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
